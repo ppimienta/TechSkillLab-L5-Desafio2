2a9f6719b71719eadec3136b041f8d20767d507b
Mishell Yagual Mendoza <mishell.yagual@sofka.com.co>
2025-10-06 15:32:38 -0500

    Initial commit

---
+++ src/streams/StreamOperation.java
@@ -0,0 +1,13 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+class StreamOperation {
+    public static void main(String[] args) {
+
+    }
+}
